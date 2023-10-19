e6644fff3cda84ec4b2e671800acace4090c2ca7
Antonio Mori <camargomoritrancoso@hotmail.com>
2023-10-19 00:32:35 -0300

    adding coments to code

--- historyTelling.java
+++ historyTelling.java
@@ -2,29 +2,36 @@
 public class historyTelling{
      
     public static void main(String[] args) {
+        // capitulo 1 e introdução
         funcoes.saida(dados.capitulos.titulo);
         funcoes.saida(dados.capitulos.cap1);
         funcoes.saida(dados.escolhas.escolha_A +"\n" + dados.escolhas.escolha_B);
         int escolha = funcoes.entradaInt("Escolha uma opção: ");
         
+        //verificacao se a escolha é valida
         while(escolha != 1 && escolha != 2){
             funcoes.saida("Opção inválida, digite novamente.");
             escolha = funcoes.entradaInt("Escolha uma opção: ");
         }
 
+        //Caminho A
         if(escolha == 1){
 
+            //capitulo A-2
             funcoes.saida(dados.capitulos.cap2);
             escolha = 0;
 
             funcoes.saida(dados.escolhas.escolha_A1 + "\n" + dados.escolhas.escolha_A2);
             escolha = funcoes.entradaInt("Escolha uma opção: ");
            
+            //verificacao se a escolha é valida
             while(escolha != 1 && escolha != 2){
                 funcoes.saida("Opção inválida, digite novamente.");
                 escolha = funcoes.entradaInt("Escolha uma opção: ");
             }
             
+
+            //Finais A
             if(escolha == 1){
                 funcoes.saida(dados.finais.final_A1);
             }
@@ -34,19 +41,23 @@ public class historyTelling{
 
         }
 
+        //Caminho B
          else if (escolha == 2){
 
+            //capitlo B-2
             funcoes.saida(dados.capitulos.cap2);
             escolha = 0;
             
             funcoes.saida(dados.escolhas.escolha_B1 + "\n" + dados.escolhas.escolha_B2);
             escolha = funcoes.entradaInt("Escolha uma opção: ");
-
+            
+            //verificacao se a escolha é valida
             while(escolha != 1 && escolha != 2){
                 funcoes.saida("Opção inválida, digite novamente.");
                 escolha = funcoes.entradaInt("Escolha uma opção: ");
             }
 
+            //Finais B
             if(escolha == 1){
                 funcoes.saida(dados.finais.final_B1);
             }
